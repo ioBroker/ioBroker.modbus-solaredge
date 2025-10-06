a7a73b7a07e57002053630da6b0e0c44c84a5686
GermanBluefox <dogafox@gmail.com>
2025-10-06 23:24:48 +0200

    Adding legacy tests

---
+++ test/slave.js
@@ -0,0 +1,49 @@
+const { server } = require('jsmodbus');
+const { Server } = require('node:net');
+const { tsv2registers } = require('@iobroker/modbus');
+
+const registers = tsv2registers('holdingRegs', `${__dirname}/../data/holding-registers.tsv`);
+let netServer;
+let modbusServer;
+
+function start() {
+    netServer = new Server();
+    modbusServer = new server.TCP(netServer);
+
+    registers.forEach((register, i) => {
+        const address = parseInt(register._address, 10) - 40001;
+        if (register.type === 'uint32be') {
+            modbusServer.holding.writeUInt32BE(i + 1, address * 2);
+        } else if (register.type === 'uint16be') {
+            modbusServer.holding.writeUInt16BE(i + 1, address * 2);
+        } else if (register.type === 'string') {
+            // Create a string with length of register.len
+            const str = `Str${i + 1}`.padEnd(register.len - 1, '-');
+            for (let j = 0; j < str.length; j++) {
+                modbusServer.holding.writeUInt8(str.charCodeAt(j), address * 2 + j, 10);
+            }
+        }
+    });
+
+    netServer.listen(1502);
+}
+
+function stop() {
+    if (modbusServer) {
+        modbusServer.close(() => {
+            if (netServer) {
+                netServer.close();
+            }
+        });
+    } else if (netServer) {
+        netServer.close();
+    }
+}
+if (require.main === module) {
+    start();
+} else {
+    module.exports = {
+        start,
+        stop,
+    }
+}
---
+++ test/testAdapter.js
@@ -0,0 +1,140 @@
+const expect = require('chai').expect;
+const setup = require('@iobroker/legacy-testing');
+const { start, stop } = require('./slave');
+const pack = require('../package.json');
+
+let objects = null;
+let states = null;
+let onStateChanged = null;
+
+const adapterShortName = setup.adapterName.substring(setup.adapterName.indexOf('.') + 1);
+
+function checkConnectionOfAdapter(cb, counter) {
+    counter ||= 0;
+    console.log(`Try check #${counter}`);
+    if (counter > 30) {
+        cb?.('Cannot check connection');
+        return;
+    }
+
+    states.getState(`system.adapter.${adapterShortName}.0.alive`, (err, state) => {
+        if (err) {
+            console.error(err);
+        }
+        if (state?.val) {
+            cb?.();
+        } else {
+            setTimeout(() => checkConnectionOfAdapter(cb, counter + 1), 1000);
+        }
+    });
+}
+
+function readState(id) {
+    return new Promise((resolve, reject) => {
+        states.getState(id, (err, state) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(state);
+        });
+    });
+}
+
+function waitAsync(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function checkValue(id, expectedValue, maxTries) {
+    let counter = 0;
+    maxTries = 10;
+    for (let i = 0; i < maxTries; i++) {
+        const value = readState(id);
+        if (value === expectedValue) {
+            return;
+        }
+        counter++;
+        console.log(`Value of ${id} is ${value}, expected ${expectedValue}. Try ${counter}`);
+        await waitAsync(500);
+    }
+    throw new Error(`Value of ${id} is not ${expectedValue} after ${maxTries} tries`);
+}
+
+describe(`Test ${adapterShortName} adapter`, function () {
+    before(`Test ${adapterShortName} adapter: Start js-controller`, function (_done) {
+        this.timeout(600000); // because of the first installation from npm
+
+        setup.setupController(async () => {
+            const config = await setup.getAdapterConfig();
+            // enable adapter
+            config.common.enabled = true;
+            config.common.loglevel = 'debug';
+            config.native.params = {
+                port: 1502,
+                bind: 'localhost',
+            };
+
+            await setup.setAdapterConfig(config.common, config.native);
+
+            // Start Modbus server
+            start();
+
+            setup.startController(
+                true,
+                () => {},
+                (id, state) => {
+                    if (onStateChanged) {
+                        onStateChanged(id, state);
+                    }
+                },
+                (_objects, _states) => {
+                    objects = _objects;
+                    states = _states;
+                    _done();
+                },
+            );
+        });
+    });
+
+    it(`Test ${adapterShortName} adapter: Check if adapter started`, function (done) {
+        this.timeout(60000);
+        checkConnectionOfAdapter(res => {
+            if (res) {
+                console.log(res);
+            }
+            expect(res).not.to.be.equal('Cannot check connection');
+            objects.setObject(
+                'system.adapter.test.0',
+                {
+                    common: {},
+                    type: 'instance',
+                },
+                () => {
+                    states.subscribeMessage('system.adapter.test.0');
+                    done();
+                },
+            );
+        });
+    });
+
+    it.only(`Test connection to server`, async function () {
+        this.timeout(10000);
+        await checkValue(`${pack.name.replace('iobroker.', '')}.0.info.connection`, true, 20);
+    });
+
+    it.only(`Test values`, async function () {
+        this.timeout(10000);
+        await checkValue(`${pack.name.replace('iobroker.', '')}.0.holdingRegisters.1`, 1, 20);
+    });
+
+    after(`Test ${adapterShortName} adapter: Stop js-controller`, function (done) {
+        this.timeout(10000);
+
+        // Stop Modbus server
+        stop();
+
+        setup.stopController(normalTerminated => {
+            console.log(`Adapter normal terminated: ${normalTerminated}`);
+            done();
+        });
+    });
+});
